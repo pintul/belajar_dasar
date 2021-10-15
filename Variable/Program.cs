cefba84cedc45f1b96a37e32f6afd8f36f9d0e8d
pintul <fourisseptivionny@gmail.com>
2021-10-16 01:32:15 +0700

    first

---
+++ Variable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string nama;
+            Console.Write("Siapa nama kamu : ");
+            string inputNama = Console.ReadLine(); //input
+            Console.WriteLine("Nama kamu : " + inputNama); //output
+            Console.ReadKey();
+        }
+    }
+}
